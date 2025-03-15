c9b179d86d49e3694c2188305132ce77fe05d0c4
Luiz Antonio De Souza Cardoso <luiz.129922@alunosatc.edu.br>
2025-03-14 21:07:23 -0300

    terminando gerador

---
+++ src/Jogador.java
@@ -0,0 +1,13 @@
+public class Jogador {
+    int idade;
+    String posicao;
+    String nome;
+    String time;
+    int camisa;
+    public void apresentar(){
+        System.out.println(nome + " idade: "+ idade + " joga na posição: " + posicao + " no time: "+ time + " vestidno a camisa: " + camisa);
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Main {
+    static String[] nomes = {"tiago", "artur", "felipe"};
+    static String[] times = {"Vasco", "Gremio", "Criciuma"};
+    static String[] posicao = {"Atacante", "Goleiro", "Zagueiro"};
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (String nome : nomes){
+            Jogador jogador = gerarJogador(nome);
+            jogador.apresentar();
+        }
     }
+
+    static Jogador gerarJogador(String nome){
+        Random gerador = new Random();
+        Jogador jogador = new Jogador();
+        jogador.nome = nome;
+        jogador.time = times[gerador.nextInt(times.length)];
+        jogador.posicao = posicao[gerador.nextInt(posicao.length)];
+        jogador.camisa = gerador.nextInt(11);
+        jogador.idade = gerador.nextInt(18,40);
+        return jogador;
+    }
+
 }
